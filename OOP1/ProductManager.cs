bdb8571e8c29047df6e0be02a8369326d323b4dd
BeratTatli <61687407+BeratTatli@users.noreply.github.com>
2021-02-13 18:20:22 +0300

    initialize

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //Encapsulation
+        public void Add(Product product)
+        {
+            //product.ProductName = "Kamera"; //Adresteki degeri degistirdigimiz icin
+            //program.cs de productname arattıgımızda 
+            //sonucu kamera olarak verecektır
+
+            Console.WriteLine(product.ProductName + "eklendi.");
+
+
+        }
+       
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi");
+        }
+    } 
+}
