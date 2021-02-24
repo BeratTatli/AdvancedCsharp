adad6e3b17f21f26637eb11f41d3cbe999e7739c
BeratTatli <61687407+BeratTatli@users.noreply.github.com>
2021-02-24 22:24:20 +0300

    secondsending

---
+++ OOP2/CustomerManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Business classes
+    class CustomerManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+
+            
+
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Berat";
+            musteri1.Soyadi = "Tatli";
+            musteri1.TcNo = "123244354545";
+            //
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "12345";
+            musteri2.SirketAdi = "Yaypel";
+            musteri2.VergiNo = "12334534";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekle(musteri1);
+            customerManager.Ekle(musteri2);
+
+        }
+    }
+}
